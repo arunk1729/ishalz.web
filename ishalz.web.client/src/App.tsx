dcaa26a36d43228e164c4ec582c1924b1634ba0f
Arun K <arun.k105@gmail.com>
2024-08-04 15:19:43 +0530

    Initial setup

---
+++ ishalz.web.client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1 id="tabelLabel">Ishalz</h1>
+            <p>Coming soon ...</p>
+        </div>
+    );
+}
+
+export default App;
